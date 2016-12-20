036bafaff245b7fc9b8e102d58cb5bb8a15089f2
Mark Nijhof <mark.nijhof@cre8ivethought.com>
2016-12-20 05:07:39 +0100

    work arround main bug

--- examples/src/View.elm
+++ examples/src/View.elm
@@ -2,7 +2,7 @@ module View exposing (view)
 
 import Html exposing (Html)
 import Html.Lazy
-import Mdc.Html exposing (div, h1, h2, text, a, br, section, fieldset, legend)
+import Mdc.Html exposing (main_, div, h1, h2, text, a, br, section, fieldset, legend)
 import Mdc.Html.Attributes exposing (..)
 import Mdc.Html.Events exposing (onClick)
 import Mdc.Button exposing (button)
@@ -37,15 +37,15 @@ viewLazy ({route} as model) =
 homePage : Html Msg
 homePage =
   div
-  [] [ h1 [] [ text "Material Design Components" ]
-     , br [] []
-     , br [] []
-     , button [ onClick <| NavigateTo ButtonsRoute ] [ text "Buttons" ]
-     , br [] []
-     , button [ onClick <| NavigateTo CardsRoute ] [ text "Cards" ]
-     , br [] []
-     , button [ onClick <| NavigateTo ElevationsRoute ] [ text "Elevations" ]
-     ]
+    [] [ h1 [] [ text "Material Design Components" ]
+       , br [] []
+       , br [] []
+       , button [ onClick <| NavigateTo ButtonsRoute ] [ text "Buttons" ]
+       , br [] []
+       , button [ onClick <| NavigateTo CardsRoute ] [ text "Cards" ]
+       , br [] []
+       , button [ onClick <| NavigateTo ElevationsRoute ] [ text "Elevations" ]
+       ]
 
 {-| Render the 404 Page.
 -}
